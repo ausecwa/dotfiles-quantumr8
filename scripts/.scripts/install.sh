5dd65b0232a94b1402c2341ae83ee96ddb941592
quantumr8 <ross.samuel.17@gmail.com>
2020-05-30 02:00:29 -0500

    update scripts

--- scripts/.scripts/install.sh
+++ scripts/.scripts/install.sh
@@ -1,1 +1,10 @@
-# this is a test
+#!/bin/sh
+# created by quantumr8
+# Custom install script to get my normal looking terminal and programs
+
+
+# Init pacman
+./pacman.sh
+# Install dependencies
+./dependencies.sh
+
