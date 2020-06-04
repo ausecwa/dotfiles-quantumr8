0d7d288331e8d83448c4e4fd159508a5d89143ef
quantumr8 <ross.samuel.17@gmail.com>
2020-06-04 01:08:34 -0500

    script updates

--- scripts/.scripts/install.zsh
+++ scripts/.scripts/install.zsh
@@ -31,7 +31,7 @@ help_msg () {
   echo  "Options:"
   echo -e "\e[32mh  --help\e[0m     Print this help."
   echo -e "\e[32mi  --init\e[0m     Initialize pacman keys and mirrors."
-  echo -e "\e[32mp  --packages\e[0m  Skip installing required packages."
+  echo -e "\e[32mp  --packages\e[0m Skip installing required packages."
   echo -e "\e[32mq\e[0m             Quiet mode.(WIP)"
   echo -e "\e[32mz  --skipzsh\e[0m  Skip installing oh-my-zsh."
   echo "\nThank you for using quantumr8's dotfiles."
@@ -84,7 +84,7 @@ fi
 # Init pacman
 if [ "$SHOULD_INITIALIZE" -eq "1" ]; then
   echo "Running pacman.zsh..."
-  ./pacman.zsh;
+  source ./pacman.zsh;
 fi
 if [ "$INSTALL_PACKAGES" -eq "1" ]; then
   echo "Installing packages...";
@@ -98,8 +98,7 @@ fi
 
 # Stow config files
 echo "Stowing config files..."
-stow -nvt ~ bashtop fzf git neofetch pictures ranger scripts vim zsh
-echo "Stow complete."
+stow -nv --target=~ --dir=~/.dotfiles bashtop fzf git neofetch pictures ranger scripts vim zsh
 
 # Done
 echo -e "\e[32mDone."
