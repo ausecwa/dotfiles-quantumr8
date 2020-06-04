fc1ed1e739c44e55005b18741351eb1a67fe5e26
quantumr8 <ross.samuel.17@gmail.com>
2020-06-04 03:23:34 -0500

    added powerlevel10k

--- scripts/.scripts/install.zsh
+++ scripts/.scripts/install.zsh
@@ -48,6 +48,7 @@ install_ohmyzsh() {
   echo "installing oh-my-zsh..."
   cd ~
   sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
   git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
   git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
